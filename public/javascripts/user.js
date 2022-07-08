b4de575f8366934c6feb6481d62646131e06a96d
vitorschapieski <fullstackschapieski@gmail.com>
2022-07-08 20:25:00 -0300

    20:24

---
+++ public/javascripts/user.js
@@ -0,0 +1,14 @@
+/* Olho para ver senha */
+
+let btn = document.querySelector('.fa-eye')
+
+
+btn.addEventListener('click', () => {
+    let inputSenha = document.querySelector('#senha')
+
+    if(inputSenha.getAttribute('type') == 'password'){
+        inputSenha.setAttribute('type', 'text')
+    } else{
+        inputSenha.setAttribute('type', 'password')
+    }
+})
